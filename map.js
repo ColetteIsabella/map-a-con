1fd5aa1a82ea426fe10c639b6b0040df6fa9c891
Colette Isabella <coletteisabella@gmail.com>
2015-04-21 15:38:22 -0700

    better commit

---
+++ map.js
@@ -0,0 +1,35 @@
+
+$(document).ready(function() {
+
+  $(".section").change(function() {
+    var selected = $(".section option:selected").attr("value");
+    $("polygon, rect").attr("class", "");
+    $("#" + selected).attr("class", "active");
+  });
+
+
+  $(".zones").change(function() {
+    var selected = $(".zones option:selected").attr("value");
+    $("polygon, rect").attr("class", "");
+    $("#" + selected).attr("class", "active");
+  });
+
+  $(".booth").change(function(){
+    var selected = $(".booth option:selected").attr("value");
+    $("rect").attr("class", "");
+    $("#" + selected).attr("class", "active");
+  });
+
+$(document).ready(function() { $("#e1").select2();
+  $("#e3").select2({
+    minimumInputLength: 2
+  });
+});
+
+
+
+
+
+
+
+
---
+++ pax.js
@@ -0,0 +1,554 @@
+//This is where you'll need to swap out the names
+//and info for your PAX, I advise setting up a
+//spreadsheet in GoogleDocs, so it can be referred
+//to fo quicker reference.
+$(document).ready(function() {
+  $("#e10").select2({
+
+    exhibitors: [
+  { "boothNumber":"3342",
+    "exhibitorName":"Comcast",
+    "sectionNumber":"1",
+    "zoneNumber":"1",
+    "exhibitorAssistant":"Van",
+    "assistantContact":"216-926-4997",
+    "zoneLeader":"WingedVictory",
+    "leaderContact":"440-840-5487",
+    "exhibitorKeywords":"internet, Comcast" },
+
+  { "boothNumber":"3344",
+    "exhibitorName":"Corsair",
+    "sectionNumber":"1",
+    "zoneNumber":"1",
+    "exhibitorAssistant":"Van",
+    "assistantContact":"216-926-4997",
+    "zoneLeader":"WingedVictory",
+    "leaderContact":"440-840-5487",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3346",
+    "exhibitorName":"Machinima",
+    "sectionNumber":"1",
+    "zoneNumber":"1",
+    "exhibitorAssistant":"Van",
+    "assistantContact":"216-926-4997",
+    "zoneLeader":"WingedVictory",
+    "leaderContact":"440-840-5487",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3444",
+    "exhibitorName":"Klei",
+    "sectionNumber":"1",
+    "zoneNumber":"1",
+    "exhibitorAssistant":"Van",
+    "assistantContact":"216-926-4997",
+    "zoneLeader":"WingedVictory",
+    "leaderContact":"440-840-5487",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3630",
+    "exhibitorName":"Square Enix",
+    "sectionNumber":"2",
+    "zoneNumber":"1",
+    "exhibitorAssistant":"Virus",
+    "assistantContact":"203-383-3023",
+    "zoneLeader":"WingedVictory",
+    "leaderContact":"440-840-5487",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3646",
+    "exhibitorName":"S2 Games",
+    "sectionNumber":"2",
+    "zoneNumber":"1",
+    "exhibitorAssistant":"Virus",
+    "assistantContact":"203-383-3023",
+    "zoneLeader":"WingedVictory",
+    "leaderContact":"440-840-5487",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3330",
+    "exhibitorName":"Xaviant",
+    "sectionNumber":"3",
+    "zoneNumber":"1",
+    "exhibitorAssistant":"Checkmate",
+    "assistantContact":"425-773-9874",
+    "zoneLeader":"WingedVictory",
+    "leaderContact":"440-840-5487",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3625",
+    "exhibitorName":"Sony Online Entertainment",
+    "sectionNumber":"3",
+    "zoneNumber":"1",
+    "exhibitorAssistant":"Checkmate",
+    "assistantContact":"425-773-9874",
+    "zoneLeader":"WingedVictory",
+    "leaderContact":"440-840-5487",
+    "exhibitorKeywords":"sony, " },
+
+  { "boothNumber":"3834",
+    "exhibitorName":"Gunnar Optiks",
+    "sectionNumber":"3",
+    "zoneNumber":"1",
+    "exhibitorAssistant":"Checkmate",
+    "assistantContact":"425-773-9874",
+    "zoneLeader":"WingedVictory",
+    "leaderContact":"440-840-5487",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3828(Satellite)",
+    "exhibitorName":"Logitech",
+    "sectionNumber":"3",
+    "zoneNumber":"1",
+    "exhibitorAssistant":"Checkmate",
+    "assistantContact":"425-773-9874",
+    "zoneLeader":"WingedVictory",
+    "leaderContact":"440-840-5487",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3822",
+    "exhibitorName":"Sanshee",
+    "sectionNumber":"3",
+    "zoneNumber":"1",
+    "exhibitorAssistant":"Checkmate",
+    "assistantContact":"425-773-9874",
+    "zoneLeader":"WingedVictory",
+    "leaderContact":"440-840-5487",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3603, 3617",
+    "exhibitorName":"Wargaming.net",
+    "sectionNumber":"4",
+    "zoneNumber":"1",
+    "exhibitorAssistant":"WingedVictory",
+    "assistantContact":"440-840-5487",
+    "zoneLeader":"WingedVictory",
+    "leaderContact":"440-840-5487",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3808",
+    "exhibitorName":"UDON",
+    "sectionNumber":"4",
+    "zoneNumber":"1",
+    "exhibitorAssistant":"WingedVictory",
+    "assistantContact":"440-840-5487",
+    "zoneLeader":"WingedVictory",
+    "leaderContact":"440-840-5487",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3810",
+    "exhibitorName":"Flying Frog",
+    "sectionNumber":"4",
+    "zoneNumber":"1",
+    "exhibitorAssistant":"WingedVictory",
+    "assistantContact":"440-840-5487",
+    "zoneLeader":"WingedVictory",
+    "leaderContact":"440-840-5487",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3812",
+    "exhibitorName":"Plyngo",
+    "sectionNumber":"4",
+    "zoneNumber":"1",
+    "exhibitorAssistant":"WingedVictory",
+    "assistantContact":"440-840-5487",
+    "zoneLeader":"WingedVictory",
+    "leaderContact":"440-840-5487",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3403",
+    "exhibitorName":"Twitch TV",
+    "sectionNumber":"5",
+    "zoneNumber":"1",
+    "exhibitorAssistant":"Evanthesquirrel",
+    "assistantContact":"508-360-7335",
+    "zoneLeader":"WingedVictory",
+    "leaderContact":"440-840-5487",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3417",
+    "exhibitorName":"Pokemon",
+    "sectionNumber":"5",
+    "zoneNumber":"1",
+    "exhibitorAssistant":"Evanthesquirrel",
+    "assistantContact":"508-360-7335",
+    "zoneLeader":"WingedVictory",
+    "leaderContact":"440-840-5487",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3411",
+    "exhibitorName":"Kixeye",
+    "sectionNumber":"5",
+    "zoneNumber":"1",
+    "exhibitorAssistant":"Evanthesquirrel",
+    "assistantContact":"508-360-7335",
+    "zoneLeader":"WingedVictory",
+    "leaderContact":"440-840-5487",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3117",
+    "exhibitorName":"Skull Candy",
+    "sectionNumber":"6",
+    "zoneNumber":"2",
+    "exhibitorAssistant":"Azazel",
+    "assistantContact":"530-318-1768",
+    "zoneLeader":"Dannicus",
+    "leaderContact":"952-393-3314",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3217",
+    "exhibitorName":"Capcom",
+    "sectionNumber":"6",
+    "zoneNumber":"2",
+    "exhibitorAssistant":"Azazel",
+    "assistantContact":"530-318-1768",
+    "zoneLeader":"Dannicus",
+    "leaderContact":"952-393-3314",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3010, 3020",
+    "exhibitorName":"SuperGiant Games",
+    "sectionNumber":"7",
+    "zoneNumber":"2",
+    "exhibitorAssistant":"Dannicus",
+    "assistantContact":"952-393-3314",
+    "zoneLeader":"Dannicus",
+    "leaderContact":"952-393-3314",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3002",
+    "exhibitorName":"Spyparty",
+    "sectionNumber":"7",
+    "zoneNumber":"2",
+    "exhibitorAssistant":"Dannicus",
+    "assistantContact":"952-393-3314",
+    "zoneLeader":"Dannicus",
+    "leaderContact":"952-393-3314",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3003, 3004",
+    "exhibitorName":"The Behemoth",
+    "sectionNumber":"7",
+    "zoneNumber":"2",
+    "exhibitorAssistant":"Dannicus",
+    "assistantContact":"952-393-3314",
+    "zoneLeader":"Dannicus",
+    "leaderContact":"952-393-3314",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3103",
+    "exhibitorName":"Intel",
+    "sectionNumber":"8",
+    "zoneNumber":"2",
+    "exhibitorAssistant":"Mori-Neko",
+    "assistantContact":"310-713-3225",
+    "zoneLeader":"Dannicus",
+    "leaderContact":"952-393-3314",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3209",
+    "exhibitorName":"Uber Entertainment",
+    "sectionNumber":"8",
+    "zoneNumber":"2",
+    "exhibitorAssistant":"Mori-Neko",
+    "assistantContact":"310-713-3225",
+    "zoneLeader":"Dannicus",
+    "leaderContact":"952-393-3314",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"3203",
+    "exhibitorName":"Gaems",
+    "sectionNumber":"8",
+    "zoneNumber":"2",
+    "exhibitorAssistant":"Mori-Neko",
+    "assistantContact":"310-713-3225",
+    "zoneLeader":"Dannicus",
+    "leaderContact":"952-393-3314",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"Sky 9",
+    "exhibitorName":"ThinkGeek",
+    "sectionNumber":"8",
+    "zoneNumber":"2",
+    "exhibitorAssistant":"Mori-Neko",
+    "assistantContact":"310-713-3225",
+    "zoneLeader":"Dannicus",
+    "leaderContact":"952-393-3314",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"Sky 7",
+    "exhibitorName":"Chessex",
+    "sectionNumber":"8",
+    "zoneNumber":"2",
+    "exhibitorAssistant":"Mori-Neko",
+    "assistantContact":"310-713-3225",
+    "zoneLeader":"Dannicus",
+    "leaderContact":"952-393-3314",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"Sky 1",
+    "exhibitorName":"Mayfair Games",
+    "sectionNumber":"9",
+    "zoneNumber":"2",
+    "exhibitorAssistant":"Mr Sandman",
+    "assistantContact":"425-770-5635",
+    "zoneLeader":"Dannicus",
+    "leaderContact":"952-393-3314",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"Sky 2",
+    "exhibitorName":"White Lotus Wishes",
+    "sectionNumber":"9",
+    "zoneNumber":"2",
+    "exhibitorAssistant":"Mr Sandman",
+    "assistantContact":"425-770-5635",
+    "zoneLeader":"Dannicus",
+    "leaderContact":"952-393-3314",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"Sky 5",
+    "exhibitorName":"Privateer Press",
+    "sectionNumber":"9",
+    "zoneNumber":"2",
+    "exhibitorAssistant":"Mr Sandman",
+    "assistantContact":"425-770-5635",
+    "zoneLeader":"Dannicus",
+    "leaderContact":"952-393-3314",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"Sky 10",
+    "exhibitorName":"Dark Horse",
+    "sectionNumber":"9",
+    "zoneNumber":"2",
+    "exhibitorAssistant":"Mr Sandman",
+    "assistantContact":"425-770-5635",
+    "zoneLeader":"Dannicus",
+    "leaderContact":"952-393-3314",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"Sky 3",
+    "exhibitorName":"Geek Chic HQ",
+    "sectionNumber":"9",
+    "zoneNumber":"2",
+    "exhibitorAssistant":"Mr Sandman",
+    "assistantContact":"425-770-5635",
+    "zoneLeader":"Dannicus",
+    "leaderContact":"952-393-3314",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"2046, 2030, 2014",
+    "exhibitorName":"Bethesda",
+    "sectionNumber":"10",
+    "zoneNumber":"3",
+    "exhibitorAssistant":"LordOwen",
+    "assistantContact":"336-688-8019",
+    "zoneLeader":"Loki",
+    "leaderContact":"248-535-7778",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"1642",
+    "exhibitorName":"Wizards of the Coast",
+    "sectionNumber":"10",
+    "zoneNumber":"3",
+    "exhibitorAssistant":"LordOwen",
+    "assistantContact":"336-688-8019",
+    "zoneLeader":"Loki",
+    "leaderContact":"248-535-7778",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"814, 1414",
+    "exhibitorName":"2K Games",
+    "sectionNumber":"11",
+    "zoneNumber":"3",
+    "exhibitorAssistant":"Kurpav",
+    "assistantContact":"425-443-8775",
+    "zoneLeader":"Loki",
+    "leaderContact":"248-535-7778",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"1814",
+    "exhibitorName":"Logitech",
+    "sectionNumber":"11",
+    "zoneNumber":"3",
+    "exhibitorAssistant":"Kurpav",
+    "assistantContact":"425-443-8775",
+    "zoneLeader":"Loki",
+    "leaderContact":"248-535-7778",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"1230",
+    "exhibitorName":"6 ft",
+    "sectionNumber":"12",
+    "zoneNumber":"3",
+    "exhibitorAssistant":"Valkyrion",
+    "assistantContact":"401-787-4982",
+    "zoneLeader":"Loki",
+    "leaderContact":"248-535-7778",
+    "exhibitorKeywords":"six, feet, foot" },
+
+  { "boothNumber":"1430",
+    "exhibitorName":"MSI",
+    "sectionNumber":"12",
+    "zoneNumber":"3",
+    "exhibitorAssistant":"Valkyrion",
+    "assistantContact":"401-787-4982",
+    "zoneLeader":"Loki",
+    "leaderContact":"248-535-7778",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"1640",
+    "exhibitorName":"WB Games",
+    "sectionNumber":"12",
+    "zoneNumber":"3",
+    "exhibitorAssistant":"Valkyrion",
+    "assistantContact":"401-787-4982",
+    "zoneLeader":"Loki",
+    "leaderContact":"248-535-7778",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"1246",
+    "exhibitorName":"Alienware",
+    "sectionNumber":"13",
+    "zoneNumber":"3",
+    "exhibitorAssistant":"Loki",
+    "assistantContact":"248-535-7778",
+    "zoneLeader":"Loki",
+    "leaderContact":"248-535-7778",
+    "exhibitorKeywords":"" },
+
+  { "boothNumber":"1240",
+    "exhibitorName":"EVGA",
+    "sectionNumber":"13",
+    "zoneNumber":"3",
+    "exhibitorAssistant":"Loki",
+    "assistantContact":"248-535-7778",
+    "zoneLeader":"Loki",
+    "leaderContact":"248-535-7778",
+    "exhibitorKeywords":"" },
+
+  { boothNumber:"1438",
+    exhibitorName:"Sandisk",
+    sectionNumber:"13",
+    zoneNumber:"3",
+    exhibitorAssistant:"Loki",
+    assistantContact:"248-535-7778",
+    zoneLeader:"Loki",
+    leaderContact:"248-535-7778",
+    exhibitorKeywords:"" },
+
+
+];
+});
+});
+
+/*for (i=0; i<exhibitors.length; i++) {
+  var newBox = document.createElement("div");
+  newBox.setAttribute("id", "section" + i);
+  newBox.setAttribute("class", "section");
+  var pTop = exhibitors[i].posTop;
+  var pRight = exhibitors[i].posRight;
+  var pBottom = exhibitors[i].posBottom;
+  var pLeft = exhibitors[i].posLeft;
+  var pWidth = pRight - pLeft;
+  var pHeight = pBottom - pTop;
+  newBox.setAttribute("style", "position:absolute; left:" + pLeft + "px; top:" + pTop + "px; width:" + pWidth + "px; height:" + pHeight + "px; z-index:9000;");
+  document.body.appendChild(newBox);
+}
+
+
+
+window.showMatches = function() {
+  clearError();
+  var hitCounter = 0;
+  clearMatches();
+  var searchString = document.getElementById("match").value;
+  for (i=0; i<exhibitors.length; i++) {
+    if ((searchString.trim() !== "") && ((exhibitors[i].boothNumber == searchString) || (exhibitors[i].exhibitorName.toLowerCase().indexOf(searchString.toLowerCase()) !== -1) || (exhibitors[i].exhibitorKeywords.toLowerCase().indexOf(searchString.toLowerCase()) !== -1))) {
+      hitCounter++;
+      document.getElementById(i).style.display = "block";
+    }
+  }
+  if (hitCounter > 0) {
+    clearForm();
+  } else {
+    if(searchString.trim() != "") document.getElementById("error").style.display = "block";
+
+  }
+}
+
+window.clearMatches = function() {
+  for (i=0; i<exhibitors.length; i++) {
+    document.getElementById(i).style.display = "none";
+  }
+}
+//for (i=0; i<exhibitors.length; i++) {
+   // document.write("<area shape='rect' coords='" + exhibitors[i].posLeft + "," + exhibitors[i].posTop + "," + exhibitors[i].posRight + "," + exhibitors[i].posBottom + "' onclick='highlightBooth(" + i + ");' title='#" + exhibitors[i].boothNumber + " " + exhibitors[i].exhibitorName + "' alt='#" + exhibitors[i].boothNumber + " " + exhibitors[i].exhibitorName + "'>");
+//  }
+function highlightBooth(boothID)  {
+  var hlDiv = document.getElementById("highlight");
+
+//  hlDiv.style.width = (exhibitors[boothID].posRight - exhibitors[boothID].posLeft - 9) + "px";
+//  hlDiv.style.height = (exhibitors[boothID].posBottom - exhibitors[boothID].posTop - 9) + "px";
+//  hlDiv.style.top = (exhibitors[boothID].posTop -4) + "px";
+//  hlDiv.style.left = (exhibitors[boothID].posLeft - 4) + "px";
+  hlDiv.style.display = "block";
+  document.getElementById("keywords").textContent = exhibitors[boothID].exhibitorName + " [" + exhibitors[boothID].exhibitorKeywords + "]";
+
+  document.getElementById("xaname").textContent = exhibitors[boothID].exhibitorAssistant;
+  document.getElementById("xaphone").innerHTML = "<a href='tel:" + exhibitors[boothID].assistantContact + "'>" + exhibitors[boothID].assistantContact + "</a>";
+  document.getElementById("zlname").textContent = exhibitors[boothID].zoneLeader;
+  document.getElementById("xaphone").innerHTML = "<a href='tel:" + exhibitors[boothID].leaderContact + "'>" + exhibitors[boothID].leaderContact + "</a>";
+console.log("xaname")
+};
+
+function clearHighlight() {
+  document.getElementById("highlight").style.display = "no";
+  document.getElementById("keywords").textContent = "No Exhibitor Selected";
+}
+
+function clearForm() {
+  document.getElementById("matches").value = "";
+}
+
+function clearAll() {
+  clearHighlight();
+  clearSections();
+  clearMatches();
+  clearForm();
+}
+
+function showSections() {
+  document.getElementById("allsections").style.display = "block";
+  for (i=1; i<=45; i++) {
+    document.write("<div onclick='showSection(" + i + ");'>Section " + i + "</div>");
+  }
+}
+
+function clearSections() {
+  for (i=0; i<exhibitors.length; i++) {
+    document.getElementById("section" + i).style.display ="none";
+  }
+}
+
+//for (i=1; i<=5; i++) {
+//  document.write("<div onclick='showZone(" + i + ");'>Zone " + i + "</div>");
+//}
+
+function showZones() {
+  document.getElementById("allzones").style.display = "none";
+}
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
